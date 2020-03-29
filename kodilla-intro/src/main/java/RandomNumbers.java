c7db75bd17403f55e4135e10d6c96401143e7183
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-03-29 16:37:27 +0200

    Fixes to ex. 3.2.2

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -5,16 +5,13 @@ public class RandomNumbers {
     public static void main(String[] args) {
         Random r = new Random();
 
-        int i = 0;
         int sum = 0;
         int min = 30;
         int max = 0;
 
-        while (sum < 50) {
-            int temp = r.nextInt(30);
-
+        while (sum < 5000) {
+            int temp = r.nextInt(31);
             sum = sum + temp;
-            i++;
 
             if (temp > max) {
                 max = temp;
