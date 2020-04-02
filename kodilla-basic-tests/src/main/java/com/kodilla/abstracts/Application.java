026a4cc79bdaeffb7875f7f8800b7e8406293c2e
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-04-02 17:28:32 +0200

    3.4 ABSTRACTS New classes: Animal, Dog, Duck, Application,AnimalProcessor

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/Application.java
@@ -0,0 +1,14 @@
+package com.kodilla.abstracts;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Animal dog = new Dog();
+
+        Duck duck = new Duck();
+
+        AnimalProcessor processor = new AnimalProcessor();
+        processor.process(dog);
+        processor.process(duck);
+    }
+}
