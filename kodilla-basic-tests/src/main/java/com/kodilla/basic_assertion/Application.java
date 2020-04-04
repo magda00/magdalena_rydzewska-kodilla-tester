ec9d555c6ac623411a5ea733b1d8ccd3a49809b5
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-04-04 22:37:37 +0200

    Ex-3.7 Unit tests to methods: substract and power

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -8,13 +8,31 @@ public class Application {
         int a = 5;
         int b = 2;
 
-        int sumResukt = calculator.sum(a,b);
-        boolean correct = ResultChecker.assertEquals(7, sumResukt);
+        int sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(7, sumResult);
 
         if (correct) {
-            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " +b);
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
         } else {
-            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " +b);
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int subtractResult = calculator.subtract(a, b);
+        boolean correctSubtract = ResultChecker.assertEquals(3, subtractResult);
+
+        if (correctSubtract) {
+            System.out.println("metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int powerResult = calculator.power(a);
+        boolean correctPower = ResultChecker.assertEquals(25, powerResult);
+
+        if (correctPower) {
+            System.out.println("Metoda power działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda powwer nie działa poprawnie dla liczby " + a);
         }
     }
 }
