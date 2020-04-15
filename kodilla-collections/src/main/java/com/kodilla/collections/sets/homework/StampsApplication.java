1947d205d570c9f75d9a3a04cc0eb9345f7c6f24
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-04-15 20:23:37 +0200

    Reformat

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -5,7 +5,9 @@ import java.util.Set;
 
 public class StampsApplication {
     public static void main(String[] args) {
+
         Set<Stamp> stamps = new HashSet<>();
+
         stamps.add(new Stamp("1", 2.5, 1.5, true));
         stamps.add(new Stamp("2", 3.0, 2.0, false));
         stamps.add(new Stamp("3", 1.0, 1.0, true));
@@ -13,9 +15,12 @@ public class StampsApplication {
         stamps.add(new Stamp("2", 3.0, 2.0, false));
         stamps.add(new Stamp("4", 2.2, 3.3, false));
         stamps.add(new Stamp("1", 2.5, 1.5, true));
+
         System.out.println("Set size: " + stamps.size());
+
         for (Stamp stamp : stamps) {
             System.out.println(stamp);
         }
+
     }
 }
