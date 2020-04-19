baf6049b2537dfe44c4a8d570f921d7b48c746e4
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-04-19 22:44:35 +0200

    Overridden method

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
@@ -1,19 +1,19 @@
 package com.kodilla.collections.adv.immutable;
 
-public class Book {
-    protected String author;
-    protected String title;
+public class Book {                  //add `final` before 'Book'` and no class won't extend 'Book'
+    protected String author;         //add `final` before 'String' and variable won't be changed
+    protected String title;          //add `final` before 'String' and variable won't be changed
 
     public Book(String author, String title) {
         this.author = author;
         this.title = title;
     }
 
-    public String getAuthor() {
+    public String getAuthor() {      //add `final` before 'String' and method won't be overridden
         return author;
     }
 
-    public String getTitle() {
+    public String getTitle() {        //add `final` before 'String' and method won't be overridden
         return title;
     }
 }
