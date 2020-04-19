7b56cf3f04bf9bf06782cfbbce86ddbe1b78049e
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-04-19 15:45:18 +0200

    Overridden methods equals and hashCode in Principal class (key in HashMap)

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/Principal.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/Principal.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.adv.maps.homework;
 
+import java.util.Objects;
+
 public class Principal {
     private String firstName;
     private String lastName;
@@ -15,5 +17,19 @@ public class Principal {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Principal principal = (Principal) o;
+        return Objects.equals(firstName, principal.firstName) &&
+                Objects.equals(lastName, principal.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
