04a7f28f865cef6806a35b1f58b973e097fecd4e
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-04-26 22:24:05 +0200

    Ex-6.2 Added new classes: Teacher, Student, Application

---
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Teacher.java
@@ -0,0 +1,16 @@
+package com.kodilla.optional.homework;
+
+import java.util.Objects;
+
+public class Teacher {
+    String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
