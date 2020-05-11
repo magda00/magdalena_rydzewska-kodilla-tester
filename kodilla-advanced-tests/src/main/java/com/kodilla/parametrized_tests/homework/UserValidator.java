b3329257172896d08579c38b08e161801323f1da
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-05-11 14:15:57 +0200

    Ex-7.1 Added UserValidator.class and UserValidatorTest.class.

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserValidator.java
@@ -0,0 +1,23 @@
+package com.kodilla.parametrized_tests.homework;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidator {
+    public boolean validateUserName(String username) {
+        if (username != null) {
+            return username.matches("^[a-zA-Z0-9._-]{3,}$");
+        }
+        return false;
+    }
+
+    public boolean validateEmail(String email) {
+        if (email != null && !email.isEmpty()) {
+            String regex = "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
+        return false;
+    }
+}
