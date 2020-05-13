8d68444da98d208d41fb1674c91683df5c100914
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-05-13 22:18:49 +0200

    Ex-7.2 Fixed methods in GamblingMachineTest.class. Changed CsvFilesSource

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -3,7 +3,6 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,46 +10,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GamblingMachineTest {
 
+    private GamblingMachine gamblingMachine = new GamblingMachine();
+
     @ParameterizedTest
     @CsvFileSource(resources = "/empty.csv")
-    public void shouldThrowsExceptionWhenSetIsEmpty() throws InvalidNumberException {
+    public void shouldThrowExceptionWhenSetIsEmpty() {
+        //Given
         Set<Integer> userNumbers = new HashSet<>();
-        assertThrows(InvalidNumberException.class, GamblingMachine.howManyWins(userNumbers));
+        //When & Then
+        assertThrows(InvalidNumberException.class, () -> gamblingMachine.howManyWins(userNumbers));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/twoUserNumbers.csv", numLinesToSkip = 1)
-    public void shouldThrowsExceptionWhenSetHasInvalidSize(int one, int two) throws InvalidNumberException {
-        Set<Integer> userNumbers = new HashSet<>(one, two);
-        assertThrows(InvalidNumberException.class, GamblingMachine.howManyWins(userNumbers));
+    public void shouldThrowExceptionWhenSetHasInvalidSize(int one, int two) {
+        //Given
+        Set<Integer> userNumbers = Set.of(one, two);
+        //When & Then
+        assertThrows(InvalidNumberException.class, () -> gamblingMachine.howManyWins(userNumbers));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/sixUserNumbersWithZero.csv", numLinesToSkip = 1)
-    public void shouldThrowsExceptionWhenSetContainsZero(int one, int two, int three, int four, int five, int six) throws InvalidNumberException {
-        Set<Integer> userNumbers = new HashSet<>(one, two, three, four, five, six);
-        assertThrows(InvalidNumberException.class, GamblingMachine.howManyWins(userNumbers));
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/sixUserNumbersWithNumbersOutsideRange.csv", numLinesToSkip = 1)
-    public void shouldThrowsExceptionWhenSetContainsNumbersOutsideRange(int one, int two, int three, int four, int five, int six) throws InvalidNumberException {
-        Set<Integer> userNumbers = new HashSet<>(one, two, three, four, five, six);
-        assertThrows(InvalidNumberException.class, GamblingMachine.howManyWins(userNumbers));
+    public void shouldThrowExceptionWhenSetContainsZero(int one, int two, int three, int four, int five, int six) {
+        //Given
+        Set<Integer> userNumbers = Set.of(one, two, three, four, five, six);
+        //When & Then
+        assertThrows(InvalidNumberException.class, () -> gamblingMachine.howManyWins(userNumbers));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/sixUserNumbersWithNegativeNumber.csv", numLinesToSkip = 1)
-    public void shouldThrowsExceptionWhenSetContainsNegativeNumber(int one, int two, int three, int four, int five, int six) throws InvalidNumberException {
-        Set<Integer> userNumbers = new HashSet<>(one, two, three, four, five, six);
-        assertThrows(InvalidNumberException.class, GamblingMachine.howManyWins(userNumbers));
+    public void shouldThrowExceptionWhenSetContainsNegativeNumber(int one, int two, int three, int four, int five, int six) {
+        //Given
+        Set<Integer> userNumbers = Set.of(one, two, three, four, five, six);
+        //When & Then
+        assertThrows(InvalidNumberException.class, () -> gamblingMachine.howManyWins(userNumbers));
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/sixCorrectUserNumbers.csv", numLinesToSkip = 1)
-    public void shouldThrowsExceptionWhenSetContainsNegativeNumber(int one, int two, int three, int four, int five, int six) throws InvalidNumberException {
-        Set<Integer> userNumbers = new HashSet<>(one, two, three, four, five, six);
-        int count = GamblingMachine.howManyWins(userNumbers);
+    public void shouldCountHowManyWinsWhenSetContainsSixCorrectNumbers(int one, int two, int three, int four, int five, int six) throws InvalidNumberException {
+        //Given
+        Set<Integer> userNumbers = Set.of(one, two, three, four, five, six);
+        //When
+        int count = gamblingMachine.howManyWins(userNumbers);
+        //Then
         assertTrue(count >= 1 && count <= 6);
     }
 }
