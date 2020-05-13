93b0eb01572f7a64364bb5d9c6f964318ed08c15
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-05-13 10:36:36 +0200

    Changed source in test to @MethodSource. Added method creating arguments

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTest.java
@@ -3,12 +3,14 @@ package com.kodilla.parametrized_tests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringManipulatorTest {
 
     private StringManipulator manipulator = new StringManipulator();
+
 
     @ParameterizedTest
     @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
@@ -17,7 +19,7 @@ class StringManipulatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null,4", "A,1"})
+    @MethodSource(value = "com.kodilla.parametrized_tests.StringSource#provideStringsForTestingLength")
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
         assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
     }
