c1a885f226e26c3541d724dbaad97ee49c62e35e
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-05-29 11:43:54 +0200

    Added interface MessageService and classes: FacebookMessageService, EmailMessageService and TextMessageService; Modified SkypeMessageService.class and SimpleApplication.class

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,17 +1,18 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Component
 public class SimpleApplication {
 
-    private SkypeMessageService messageService;
+    @Resource(name = "skypeMessageService")
+    private MessageService messageService;
 
-    @Autowired
-    public void setMessageService(SkypeMessageService messageService) {
-        this.messageService = messageService;
-    }
+ //   public SimpleApplication(MessageService messageService) {
+  //      this.messageService = messageService;
+  //  }
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
