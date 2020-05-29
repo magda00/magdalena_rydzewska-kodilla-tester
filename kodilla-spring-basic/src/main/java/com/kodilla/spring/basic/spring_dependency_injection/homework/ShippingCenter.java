3881a2caed28bf4abdde2a38a2f3b15ef29a0bab
Magdalena Rydzewska <magdalenarydzewska13@gmail.com>
2020-05-29 13:21:40 +0200

    Ex-9.3 Added package homework in com.kodilla.spring.basic.spring_dependency_injection (kodilla-spring-basic module) and classes: NotificationService, DeliveryService, ShippingCenter and ShippingCenterTest

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
@@ -0,0 +1,24 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShippingCenter {
+
+    @Autowired
+    @Qualifier(value = "notificationService")
+    private NotificationService notificationService;
+
+    @Autowired
+    @Qualifier(value = "deliveryService")
+    private DeliveryService deliveryService;
+
+    public String sendPackage(String address, double weight) {
+        if (deliveryService.deliverPackage(address, weight)) {
+            return notificationService.success(address);
+        }
+        return notificationService.fail(address);
+    }
+}
